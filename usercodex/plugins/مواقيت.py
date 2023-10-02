04f33b40e3ae1f54139bf72aba248bcbddce35d0
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-10-02 04:39:47 +0300

    Update مواقيت.py

--- usercodex/plugins/مواقيت.py
+++ usercodex/plugins/مواقيت.py
@@ -31,15 +31,15 @@ async def get_adzan(adzan):
             adzan, f"`Couldn't fetch any data about the city {LOKASI}`", 5
         )
     result = json.loads(request.text)
-    codresult = f"<b>**❒:أوقات الصلوات الإسلامية ** </b>\
-            \n\n<b>**❂:مدينة**  : </b><i>{result['query']}</i>\
-            \n<b>**❂:لدولة**   : </b><i>{result['country']}</i>\
-            \n<b>**❂:التاريخ**  : </b><i>{result['items'][0]['date_for']}</i>\
-            \n<b>**❂:الفجر**  : </b><i>{result['items'][0]['fajr']}</i>\
-            \n<b>**❂:االصباح*  : </b><i>{result['items'][0]['shurooq']}</i>\
-            \n<b>**❂:الضهر**  : </b><i>{result['items'][0]['dhuhr']}</i>\
-            \n<b>**❂:العصر**  : </b><i>{result['items'][0]['asr']}</i>\
-            \n<b>**❂:المغرب** : </b><i>{result['items'][0]['maghrib']}</i>\
-            \n<b>**❂:الفجر**  : </b><i>{result['items'][0]['isha']}</i>\
+    codresult = f"<b>**❒ : أوقات الصلوات الإسلامية ** </b>\
+            \n\n<b>❂ : مدينة :      </b>   <i>{result['query']}</i>\
+            \n<b>❂ : لدولة  :    </b>   <i>{result['country']}</i>\
+            \n<b>❂ : التاريخ  :  </b>   <i>{result['items'][0]['date_for']}</i>\
+            \n<b>❂ : الفجر :    </b>   <i>{result['items'][0]['fajr']}</i>\
+            \n<b>❂ : االصباح :   </b>   <i>{result['items'][0]['shurooq']}</i>\
+            \n<b>❂ : الضهر  :    </b>   <i>{result['items'][0]['dhuhr']}</i>\
+            \n<b>❂ : العصر  :     </b>   <i>{result['items'][0]['asr']}</i>\
+            \n<b>❂ : المغرب :   </b>   <i>{result['items'][0]['maghrib']}</i>\
+            \n<b>❂ : الفجر  :    </b>  <i>{result['items'][0]['isha']}</i>\
     "
     await edit_or_reply(adzan, codresult, "html")
