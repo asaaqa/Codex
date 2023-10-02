40d9e9309804e9351d9e554e0176b29fc7262a49
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-10-03 02:39:43 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -157,7 +157,7 @@ class Config(object):
     COUNTRY = str(os.environ.get("COUNTRY", ""))
     TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
     # For updater plugin
-    UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "Zade")
+    UPSTREAM_REPO_BRANCH = os.environ.get("UPSTREAM_REPO_BRANCH", "master")
     # dont touch this at all
     SUDO_USERS: Set[int] = set()
     CODLOGO = None
