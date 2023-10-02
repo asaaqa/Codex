b64c9d8c0da9240ed59e91f448c8a8b378347e99
asaaqa <134251851+asaaqa@users.noreply.github.com>
2023-10-02 05:23:20 +0300

    Update and rename alive.py to الفحص.py

--- usercodex/plugins/الفحص.py
+++ usercodex/plugins/الفحص.py
@@ -18,14 +18,14 @@ from ..helpers.utils import reply_id
 from ..sql_helper.globals import gvarstatus
 from . import StartTime, codex, codversion, mention
 
-plugin_category = "utils"
+plugin_category = "العروض"
 
 
 @codex.cod_cmd(
-    pattern="alive$",
-    command=("alive", plugin_category),
+    pattern="بنج$",
+    command=("بنج", plugin_category),
     info={
-        "header": "To check bot's alive status",
+        "header": "❂ :  لفحص بوت الأمبـراطور ",
         "options": "To show media in this cmd you need to set ALIVE_PIC with media link, get this by replying the media by .tgm",
         "usage": [
             "{tr}alive",
@@ -38,18 +38,19 @@ async def amireallyalive(event):
     uptime = await get_readable_time((time.time() - StartTime))
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "✧✧"
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "✮ MY BOT IS RUNNING SUCCESSFULLY ✮"
-    COD_IMG = gvarstatus("ALIVE_PIC")
+    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "✮ -  بـوت  𝑨𝑴𝑩𝑹𝑶   يعمـل .. جيداً "
+    COD_IMG = gvarstatus("ALIVE_PIC") or "https://telegra.ph/file/8822bd4624d64b4ab67cf.jpg" 
     if COD_IMG:
         A_IMG = [x for x in COD_IMG.split()]
         PIC = random.choice(A_IMG)
         cod_caption = f"**{ALIVE_TEXT}**\n\n"
-        cod_caption += f"**{EMOJI} User : {mention}**\n"
-        cod_caption += f"**{EMOJI} Uptime :** `{uptime}\n`"
-        cod_caption += f"**{EMOJI} Telethon version :** `{version.__version__}\n`"
-        cod_caption += f"**{EMOJI} Codexuserbot Version :** `{codversion}`\n"
-        cod_caption += f"**{EMOJI} Python Version :** `{python_version()}\n`"
-        cod_caption += f"**{EMOJI} Database :** `{check_sgnirts}`\n"
+        cod_caption += f"**{EMOJI} المستخدم : {mention}**\n"
+        cod_caption += f"**{EMOJI} الوقت :** `{uptime}\n`"
+        cod_caption += f"**{EMOJI} إصدار تليثيون :** `{version.__version__}\n`"
+        cod_caption += f"**{EMOJI}إصدار 𝑨𝑴𝑩𝑹𝑶 :** `{codversion}`\n"
+        cod_caption += f"**{EMOJI}  إصدار باثيون :** `{python_version()}\n`"
+        cod_caption += f"**{EMOJI} قاعدة البيانات :** `{check_sgnirts}`\n"
+        cod_caption += f"**{EMOJI} قناة السورس :** [𝗦𝗢𝗨𝗥𝗖𝗘𖢏𝗘𝗠𝗣𝗘𝗥𝗢𝗥](t.me/Mlze1bot) \n"
         try:
             await event.client.send_file(
                 event.chat_id, PIC, caption=cod_caption, reply_to=reply_to_id
@@ -64,35 +65,36 @@ async def amireallyalive(event):
         await edit_or_reply(
             event,
             f"**{ALIVE_TEXT}**\n\n"
-            f"**{EMOJI} User : {mention}**\n"
-            f"**{EMOJI} Uptime :** `{uptime}\n`"
-            f"**{EMOJI} Telethon Version :** `{version.__version__}\n`"
-            f"**{EMOJI} Codexuserbot Version :** `{codversion}`\n"
-            f"**{EMOJI} Python Version :** `{python_version()}\n`"
-            f"**{EMOJI} Database :** `{check_sgnirts}`\n",
+            f"**{EMOJI} المستخدم : {mention}**\n"
+            f"**{EMOJI} الوقت :** `{uptime}\n`"
+            f"**{EMOJI} إصدار تليثيون  :** `{version.__version__}\n`"
+            f"**{EMOJI} إصدار 𝑨𝑴𝑩𝑹𝑶 :** `{codversion}`\n"
+            f"**{EMOJI} إصدار باثيون :** `{python_version()}\n`"
+            f"**{EMOJI}  قاعدة البيانات :** `{check_sgnirts}`\n"
+            f"**{EMOJI} قناة السورس :** [𝗦𝗢𝗨𝗥𝗖𝗘𖢏𝗘𝗠𝗣𝗘𝗥𝗢𝗥](t.me/Mlze1bot) \n",
         )
 
 
 @codex.cod_cmd(
-    pattern="ialive$",
-    command=("ialive", plugin_category),
+    pattern="فحص$",
+    command=("فحص", plugin_category),
     info={
         "header": "To check bot's alive status via inline mode",
         "options": "To show media in this cmd you need to set ALIVE_PIC with media link, get this by replying the media by .tgm",
         "usage": [
-            "{tr}alive",
+            "{tr}فحص",
         ],
     },
 )
 async def amireallyalive(event):
-    "A kind of showing bot details by your inline bot"
+    "نوع من عرض تفاصيل الروبوت بواسطة الروبوت المضمن الخاص بك"
     reply_to_id = await reply_id(event)
     EMOJI = gvarstatus("ALIVE_EMOJI") or "✧✧"
-    cod_caption = f"**Codexuserbot is Up and Running**\n"
-    cod_caption += f"**{EMOJI} Telethon version :** `{version.__version__}\n`"
-    cod_caption += f"**{EMOJI} Codexuserbot Version :** `{codversion}`\n"
-    cod_caption += f"**{EMOJI} Python Version :** `{python_version()}\n`"
-    cod_caption += f"**{EMOJI} User:** {mention}\n"
+    cod_caption = f"**✮ -  بـوت  𝑨𝑴𝑩𝑹𝑶   يعمـل .. جيداً    **\n"
+    cod_caption += f"**{EMOJI}  إصدار تليثيون :** `{version.__version__}\n`"
+    cod_caption += f"**{EMOJI} إصدار 𝑨𝑴𝑩𝑹𝑶 :** `{codversion}`\n"
+    cod_caption += f"**{EMOJI} إصدار باثيون :** `{python_version()}\n`"
+    cod_caption += f"**{EMOJI} المستخدم:** {mention}\n"
     results = await event.client.inline_query(Config.TG_BOT_USERNAME, cod_caption)
     await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
     await event.delete()
